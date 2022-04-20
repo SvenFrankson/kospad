7c9177bfb4375a1fc373928e50c2a266e671737f
SvenFrankson <sven.frankson@gmail.com>
2022-04-21 01:28:24 +0200

    Add Skybox

--- scripts/Main.ts
+++ scripts/Main.ts
@@ -35,8 +35,24 @@ class Main {
 		this.scene.clearColor.copyFromFloats(158 / 255, 86 / 255, 55 / 255, 1);
 
 		this.camera = new BABYLON.ArcRotateCamera("camera", - Math.PI / 4, Math.PI / 4, 20, BABYLON.Vector3.Zero(), this.scene);
+        this.camera.attachControl(this.canvas);
 		
 		BABYLON.Engine.ShadersRepository = "./shaders/";
+
+        let skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 2000.0 }, this.scene);
+		skybox.rotation.y = Math.PI / 2;
+		skybox.infiniteDistance = true;
+		let skyboxMaterial: BABYLON.StandardMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
+		skyboxMaterial.backFaceCulling = false;
+		let skyboxTexture = new BABYLON.CubeTexture(
+			"./assets/skyboxes/sky",
+			this.scene,
+			["-px.png", "-py.png", "-pz.png", "-nx.png", "-ny.png", "-nz.png"]);
+		skyboxMaterial.reflectionTexture = skyboxTexture;
+		skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+		skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+		skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+		skybox.material = skyboxMaterial;
 	}
 
     public animate(): void {
