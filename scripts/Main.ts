9cb71ed40fe62e9600332be5eb5843b1f6fded97
SvenFrankson <sven.frankson@gmail.com>
2023-02-09 13:05:57 +0100

    Octree now serialize / deserialize ok.

--- scripts/Main.ts
+++ scripts/Main.ts
@@ -2,12 +2,41 @@ class Main {
 
     constructor() {
 		let node = new OctreeNode<number>();
-		let ii = Math.floor(Math.random() * 16);
-		let jj = Math.floor(Math.random() * 16);
-		let kk = Math.floor(Math.random() * 16);
-		console.log("value = " + node.get(ii, jj, kk));
-		node.set(6, ii, jj, kk);
-		console.log("value = " + node.get(ii, jj, kk));
+		for (let n = 0; n < 1024; n++) {
+			let v = Math.floor(Math.random() * 10);
+			let i = Math.floor(Math.random() * 8);
+			let j = Math.floor(Math.random() * 8);
+			let k = Math.floor(Math.random() * 8);
+			//node.set(n, n, n, n);
+			node.set(v, i, j, k);
+		}
+		/*
+		console.log(node);
+		let serial = node.serialize();
+		console.log("3: " + serial[3]);
+		console.log("2: " + serial[2]);
+		console.log("1: " + serial[1]);
+
+		console.log(" ");
+
+		let deserial = OctreeNode.Deserialize(serial);
+		let newSerial = deserial.serialize();
+		console.log("3: " + newSerial[3]);
+		console.log("2: " + newSerial[2]);
+		console.log("1: " + newSerial[1]);
+
+		console.log(" ");
+
+		for (let i = 1; i <= 3; i++) {
+			console.log(i + ": " + (serial[i] === newSerial[i]));
+		}
+		*/
+		let serial = node.serializeToString();
+		console.log(serial);
+		let deserial = OctreeNode.DeserializeFromString(serial);
+		let newSerial = deserial.serializeToString();
+		console.log(newSerial);
+		console.log(serial === newSerial);
 	}
 }
 
--- scripts/Octree.ts
+++ scripts/Octree.ts
@@ -3,37 +3,34 @@ class OctreeNode<T> {
     public size: number;
     public degree: number = 3;
     public parent: OctreeNode<T>;
-    public children: any[][][];
+    public children: any[];
 
     constructor(parent?: OctreeNode<T>) {
         if (parent) {
             this.parent = parent;
             this.degree = parent.degree - 1;
         }
-        this.size = Math.pow(2, this.degree);
+        this.size = Math.pow(2, this.degree - 1);
     }
 
     private _getChild(ii: number, jj: number, kk: number): any {
         if (this.children) {
-            if (this.children[ii]) {
-                if (this.children[ii][jj]) {
-                    return this.children[ii][jj][kk];
-                }
-            }
+            return this.children[4 * ii + 2 * jj + kk];
         }
     }
     
     private _setChild(child: any, ii: number, jj: number, kk: number): void {
-        if (!this.children) {
+        if (this.children === undefined) {
             this.children = [];
         }
-        if (!this.children[ii]) {
-            this.children[ii] = [];
+        this.children[4 * ii + 2 * jj + kk] = child;
+    }
+    
+    private _setNthChild(child: any, n: number): void {
+        if (this.children === undefined) {
+            this.children = [];
         }
-        if (!this.children[ii][jj]) {
-            this.children[ii][jj] = [];
-        }
-        this.children[ii][jj][kk] = child;
+        this.children[n] = child;
     }
 
     public get(i: number, j: number, k: number): T {
@@ -60,33 +57,163 @@ class OctreeNode<T> {
         let ii = Math.floor(i / this.size) % 2;
         let jj = Math.floor(j / this.size) % 2;
         let kk = Math.floor(k / this.size) % 2;
-        
+
         if (this.degree === 1) {
             this._setChild(v, ii, jj, kk);
         }
         else {
-            let childOctree = new OctreeNode<T>(this);
-            this._setChild(childOctree, ii, jj, kk);
+            let childOctree = this._getChild(ii, jj, kk);
+            if (!childOctree) {
+                childOctree = new OctreeNode<T>(this);
+                this._setChild(childOctree, ii, jj, kk);
+            }
             childOctree.set(v, i, j, k);
         }
     }
 
-    public serialize(output: string[]): string {
-        for (let ii = 0; ii < 2; ii++) {
-            for (let jj = 0; jj < 2; jj++) {
-                for (let kk = 0; kk < 2; kk++) {
-                    let child = this._getChild(ii, jj, kk);
-                    if (!child) {
-                        output[this.degree] += "_";
-                    }
-                    else if (child instanceof OctreeNode) {
-                        output[this.degree] += ".";
-                    }
-                    else {
-                        output[this.degree]
-                    }
+    public serializeToString(): string {
+        let output = this.serialize();
+        let compressedOutput = output[3] + "#" + output[2] + "#" + output[1];
+
+        let l1 = compressedOutput.length;
+
+        compressedOutput = compressedOutput.replaceAll("________", "H");
+        compressedOutput = compressedOutput.replaceAll("_______", "G");
+        compressedOutput = compressedOutput.replaceAll("______", "F");
+        compressedOutput = compressedOutput.replaceAll("_____", "E");
+        compressedOutput = compressedOutput.replaceAll("____", "D");
+        compressedOutput = compressedOutput.replaceAll("___", "C");
+        compressedOutput = compressedOutput.replaceAll("__", "B");
+        //compressedOutput = compressedOutput.replaceAll("_", "A");
+        
+        compressedOutput = compressedOutput.replaceAll("........", "P");
+        compressedOutput = compressedOutput.replaceAll(".......", "O");
+        compressedOutput = compressedOutput.replaceAll("......", "N");
+        compressedOutput = compressedOutput.replaceAll(".....", "M");
+        compressedOutput = compressedOutput.replaceAll("....", "L");
+        compressedOutput = compressedOutput.replaceAll("...", "K");
+        compressedOutput = compressedOutput.replaceAll("..", "J");
+        //compressedOutput = compressedOutput.replaceAll(".", "I");
+
+        let l2 = compressedOutput.length;
+
+        console.log("Compression rate " + ((l2 / l1) * 100).toFixed(0) + "%");
+
+        return compressedOutput;
+    }
+
+    public serialize(output?: string[]): string[] {
+        if (!output) {
+            output = [];
+        }
+        if (!output[this.degree]) {
+            output[this.degree] = "";
+        }
+
+        for (let n = 0; n < 8; n++) {
+            let child = this.children[n];
+            if (child === undefined) {
+                output[this.degree] += "_";
+            }
+            else if (child instanceof OctreeNode) {
+                output[this.degree] += ".";
+                child.serialize(output);
+            }
+            else {
+                output[this.degree] += child.toString();
+            }
+        }
+
+        return output;
+    }
+
+    public static DeserializeFromString(input: string): OctreeNode<number> {
+        let deCompressedInput = input;
+
+        deCompressedInput = deCompressedInput.replaceAll("H", "________");
+        deCompressedInput = deCompressedInput.replaceAll("G", "_______");
+        deCompressedInput = deCompressedInput.replaceAll("F", "______");
+        deCompressedInput = deCompressedInput.replaceAll("E", "_____");
+        deCompressedInput = deCompressedInput.replaceAll("D", "____");
+        deCompressedInput = deCompressedInput.replaceAll("C", "___");
+        deCompressedInput = deCompressedInput.replaceAll("B", "__");
+        //deCompressedInput = deCompressedInput.replaceAll("A", "_");
+        
+        deCompressedInput = deCompressedInput.replaceAll("P", "........");
+        deCompressedInput = deCompressedInput.replaceAll("O", ".......");
+        deCompressedInput = deCompressedInput.replaceAll("N", "......");
+        deCompressedInput = deCompressedInput.replaceAll("M", ".....");
+        deCompressedInput = deCompressedInput.replaceAll("L", "....");
+        deCompressedInput = deCompressedInput.replaceAll("K", "...");
+        deCompressedInput = deCompressedInput.replaceAll("J", "..");
+        //deCompressedInput = deCompressedInput.replaceAll("I", ".");
+
+        let split = deCompressedInput.split("#");
+        return OctreeNode.Deserialize([undefined, split[2], split[1], split[0]]);
+    }
+
+    public static Deserialize(input: string[]): OctreeNode<number> {
+        let node = new OctreeNode<number>();
+        
+        // degree 3
+        let inputDeg3 = input[3];
+        let degree2Nodes: OctreeNode<number>[] = [];
+        let degree1Nodes: OctreeNode<number>[] = [];
+        for (let n = 0; n < inputDeg3.length; n++) {
+            let c = inputDeg3[n];
+            if (c === ".") {
+                let newNode = new OctreeNode<number>(node);
+                degree2Nodes.push(newNode);
+                node._setNthChild(newNode, n);
+            }
+            else if (c === "_") {
+
+            }
+            else {
+                let v = parseInt(c);
+                node._setNthChild(v, n);
+            }
+        }
+
+        // degree 2
+        for (let m = 0; m < degree2Nodes.length; m++) {
+            let nodeDeg2 = degree2Nodes[m];
+            let inputDeg2 = input[2].substring(m * 8, (m + 1) * 8);
+
+            for (let n = 0; n < inputDeg2.length; n++) {
+                let c = inputDeg2[n];
+                if (c === ".") {
+                    let newNode = new OctreeNode<number>(nodeDeg2);
+                    degree1Nodes.push(newNode);
+                    nodeDeg2._setNthChild(newNode, n);
+                }
+                else if (c === "_") {
+    
+                }
+                else {
+                    let v = parseInt(c);
+                    nodeDeg2._setNthChild(v, n);
                 }
             }
         }
+
+        // degree 1
+        for (let m = 0; m < degree1Nodes.length; m++) {
+            let nodeDeg1 = degree1Nodes[m];
+            let inputDeg1 = input[1].substring(m * 8, (m + 1) * 8);
+
+            for (let n = 0; n < inputDeg1.length; n++) {
+                let c = inputDeg1[n];
+                if (c === "_") {
+    
+                }
+                else {
+                    let v = parseInt(c);
+                    nodeDeg1._setNthChild(v, n);
+                }
+            }
+        }
+
+        return node;
     }
 }
